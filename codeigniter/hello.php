01ed16b6355a82abbea36bde078c065f116291a9
ytlim <jesusloveyt@gmail.com>
2021-02-21 23:14:23 +0900

    init

---
+++ codeigniter/hello.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="node express sample by Frenz" name="description" />
+    <meta content="Frenz" name="author" />
+    <title> hello world </title>
+</head>
+
+<body>
+hello world
+</body>
+</html>
